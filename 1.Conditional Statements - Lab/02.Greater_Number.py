f511544d698564eab20e798061e91ac5e20b5b7b
D-on3 <dnedyalkov396@gmail.com>
2022-04-25 19:11:29 +0300

    first one

---
+++ 1.Conditional Statements - Lab/02.Greater_Number.py
@@ -0,0 +1,4 @@
+first_number = int(input())
+second_number = int(input())
+desider = max(first_number, second_number)
+print(desider)
---
+++ 1.Conditional Statements - Lab/03.Even_or_Odd.py
@@ -0,0 +1,5 @@
+number = int(input())
+if number % 2 == 0:
+    print('even')
+elif number % 2 == 1:
+    print('odd')
