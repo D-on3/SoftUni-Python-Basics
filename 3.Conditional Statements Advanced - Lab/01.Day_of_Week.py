29ccf48dabb42f840a2ed921df7854266ee2632a
D-on3 <dnedyalkov396@gmail.com>
2022-04-29 00:23:25 +0300

    init

---
+++ 3.Conditional Statements Advanced - Lab/01.Day_of_Week.py
@@ -0,0 +1,17 @@
+pick_a_number = int(input())
+if pick_a_number == 1:
+    print('Monday')
+elif pick_a_number == 2:
+    print('Tuesday')
+elif pick_a_number == 3:
+    print('Wednesday')
+elif pick_a_number == 4:
+    print('Thursday')
+elif pick_a_number == 5:
+    print('Friday')
+elif pick_a_number == 6:
+    print('Saturday')
+elif pick_a_number == 7:
+    print('Sunday')
+else:
+    print('Error')
