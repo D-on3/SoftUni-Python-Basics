d7b93901a10717aaac23098680fcb3d70b55da57
D-on3 <dnedyalkov396@gmail.com>
2022-04-30 01:56:33 +0300

    init

---
+++ 3.Conditional Statements Advanced - Lab/08.Cinema_Ticket.py
@@ -0,0 +1,9 @@
+day_of_the_week = input()
+
+if day_of_the_week == 'Monday' or day_of_the_week == 'Tuesday' or day_of_the_week == 'Friday':
+    print(12)
+elif day_of_the_week == 'Wednesday' or day_of_the_week == 'Thursday':
+    print(14)
+elif day_of_the_week == 'Saturday' or day_of_the_week == 'Sunday':
+    print(16)
+
